fd750d93d68e669b636787107a51c1c0b6312c21
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-11-02 16:00:04 -0500

    tested both functions isSunk and shipHit after modifieng them

--- src/shipClass.test.js
+++ src/shipClass.test.js
@@ -1,8 +1,15 @@
 import { Ship } from "./ship";
 
-test('testing shipHit', ()=> {
+test('testing isSunk', ()=> {
     const ship1 = new Ship(4,0)
     expect(ship1.isSunk()).toBe(false)
     const ship2 = new Ship(4,4)
     expect(ship2.isSunk()).toBe(true)
+})
+
+test('testing isSunk after shipHit', ()=>{
+    const ship = new Ship(3,2)
+    expect(ship.isSunk()).toBe(false)
+    ship.shipHit()
+    expect(ship.isSunk()).toBe(true)
 })
