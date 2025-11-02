48e6103f12d95219da6cd5a85985044cd6f25864
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-11-02 16:02:14 -0500

    updated tests for new constructor

--- src/shipClass.test.js
+++ src/shipClass.test.js
@@ -1,15 +1,17 @@
 import { Ship } from "./ship";
 
 test('testing isSunk', ()=> {
-    const ship1 = new Ship(4,0)
+    const ship1 = new Ship(4)
     expect(ship1.isSunk()).toBe(false)
-    const ship2 = new Ship(4,4)
+    const ship2 = new Ship(0)
     expect(ship2.isSunk()).toBe(true)
 })
 
 test('testing isSunk after shipHit', ()=>{
-    const ship = new Ship(3,2)
+    const ship = new Ship(3)
     expect(ship.isSunk()).toBe(false)
+    ship.shipHit()
+    ship.shipHit()
     ship.shipHit()
     expect(ship.isSunk()).toBe(true)
 })
