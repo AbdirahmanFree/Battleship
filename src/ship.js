80658f9c764e58aedfc2ebbf116328efe5bb7cfd
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-11-02 14:54:49 -0500

    Added shipHit and isSunk functions shipHit increases this.hit attribute by 1 each time it's called isSunk checks if a ships hit attribute is greater than or equal to it's length if so it returns true else false

--- src/ship.js
+++ src/ship.js
@@ -4,4 +4,15 @@ class Ship {
         this.hit = hit
         this.sunk = sunk
     }
+
+    shipHit(){
+        this.hit = this.hit +1;
+    }
+
+    isSunk(){
+        if(this.hit >= this.length){
+            return true
+        }
+        return false
+    }
 }
